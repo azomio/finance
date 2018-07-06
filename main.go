961a8607376bf1a153bda7e2de9378d1cafe53e1
azomio <azomio@gmail.com>
2018-07-06 09:47:29 +0300

    change receipt storage format

--- main.go
+++ main.go
@@ -223,46 +223,6 @@ func addReceiptHandler (w http.ResponseWriter, r *http.Request) {
     fmt.Println(page)
     t.Execute(w, page)
 
-    // // insert
-    // stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) values(?,?,?)")
-    // checkErr(err)
-
-    // res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
-    // checkErr(err)
-
-    // id, err := res.LastInsertId()
-    // checkErr(err)
-
-    // fmt.Println(id)
-    // // update
-    // stmt, err = db.Prepare("update userinfo set username=? where uid=?")
-    // checkErr(err)
-
-    // res, err = stmt.Exec("astaxieupdate", id)
-    // checkErr(err)
-
-    // affect, err := res.RowsAffected()
-    // checkErr(err)
-
-    // fmt.Printf(w, affect)
-
-    // // query
-
-
-    // // delete
-    // stmt, err = db.Prepare("delete from userinfo where uid=?")
-    // checkErr(err)
-
-    // res, err = stmt.Exec(id)
-    // checkErr(err)
-
-    // affect, err = res.RowsAffected()
-    // checkErr(err)
-
-    // fmt.Printf(w, affect)
-
-    // db.Close()
-
 }
 
 func checkErr(err error) {
--- sql/receipt.sql
+++ sql/receipt.sql
@@ -1,2 +1,8 @@
-CREATE TABLE IF NOT EXISTS receipt (fn,i,fp,s,t);
+CREATE TABLE IF NOT EXISTS receipt (
+	fn text,
+	i text,
+	fp text,
+	sum text,
+	time integer
+);
 CREATE UNIQUE INDEX IF NOT EXISTS receipt_ind ON receipt (fn,i,fp);
